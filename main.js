3b4881116c3295297165667a74424ce28342d1c4
emirMaya <emirmayaok@gmail.com>
2022-02-03 20:03:23 -0300

    segunda entrega proyecto final mas cosas mejoradas

--- main.js
+++ main.js
@@ -68,157 +68,100 @@ const cuenta = new Cuenta('Emir Maya', '24', 'cornu 1234', balance);
 
 
 const cryptos = [`BTC`, `ETH`, `ADA`, `DOT`, `DOGE`, `SOL`];
-const stableCoins = [`USDT`, `DAI`, `USDC`];
-let convertir = prompt(`a que desea convertir sus USDT?`).toUpperCase();
-//DEPENDE LO QUE ESCRIBA BUSCAREMOS SI ESTA LA CRYPTO O NO CON UN FIND
-let encontrar;
-do {
-    encontrar = cryptos.find(elemento => elemento === convertir);
-    if (encontrar) {
-        alert(`La criptomoneda ${convertir} está disponible`);
-    } else {
-        convertir = prompt(`La cryptomoneda no se encuentra, escriba otra`).toUpperCase();
-    };
-} while (encontrar != convertir);
 
-//Todavia no funciona pero quiero que una sola funcion convierta todas las monedas
-// if (convertir === `ETH` || convertir === `BTC` || convertir === `DOT` || convertir === `ADA` || convertir === `SOL`|| convertir === `DOGE`){
-//     max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-//         if (max == 'si') {
-//             alert(`${cuenta.convertirAEth(balance)} LUCIA con una comisión de ${comision} usdt`);
-//         } else if (max == 'no') {
-//             usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a ${convertir}`));
-//             if (usdt <= balance) {
-//                 alert(`${cuenta.convertirAEth(usdt)} ${convertir} con una comisión de ${comision} usdt`);
-//             } else {
-//                 usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a ${convertir}`));
-//                 alert(`${cuenta.convertirAEth(usdt)} ${convertir} con una comisión de ${comision} usdt`);
-//             };
-//         };
-// }
-
-switch (convertir) {
-    case `ETH`:
-        max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-        if (max == 'si') {
-            conversion = cuenta.convertirAEth(balance);
-            alert(`${cuenta.convertirAEth(balance)} ETH con una comisión de ${comision} usdt`);
-        } else if (max == 'no') {
-            usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a eth`));
-            if (usdt <= balance) {
-                conversion = cuenta.convertirAEth(usdt);
-                alert(`${cuenta.convertirAEth(usdt)} ETH con una comisión de ${comision} usdt`);
-            } else {
-                usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a eth`));
-                conversion = cuenta.convertirAEth(usdt);
-                alert(`${cuenta.convertirAEth(usdt)} ETH con una comisión de ${comision} usdt`);
-            };
-        };
-        break;
-
-    case `BTC`:
-        max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-        if (max == 'si') {
-            conversion = cuenta.convertirABtc(balance);
-            alert(`${cuenta.convertirABtc(balance)} BTC con una comisión de ${comision} usdt`);
-        } else if (max == 'no') {
-            usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a BTC`));
-            if (usdt <= balance) {
-                conversion = cuenta.convertirABtc(usdt);
-                alert(`${cuenta.convertirABtc(usdt)} BTC con una comisión de ${comision} usdt`);
-            } else {
-                usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a BTC`));
-                conversion = cuenta.convertirABtc(usdt);
-                alert(`${cuenta.convertirABtc(usdt)} BTC con una comisión de ${comision} usdt`);
-            };
-        };
-        break;
-    case `ADA`:
-        max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-        if (max == 'si') {
-            conversion = cuenta.convertirAAda(balance);
-            alert(`${cuenta.convertirAAda(balance)} ADA con una comisión de ${comision} usdt`);
-        } else if (max == 'no') {
-            usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a ADA`));
-            if (usdt <= balance) {
-                cuenta.convertirAAda(usdt);
-                alert(`${cuenta.convertirAAda(usdt)} ADA con una comisión de ${comision} usdt`);
-            } else {
-                usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a ADA`));
-                cuenta.convertirAAda(usdt);
-                alert(`${cuenta.convertirAAda(usdt)} ADA con una comisión de ${comision} usdt`);
-            };
-        };
-        break;
-
-    case `DOGE`:
-        max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-        if (max == 'si') {
-            conversion = cuenta.convertirADoge(balance);
-            alert(`${cuenta.convertirADoge(balance)} DOGE con una comisión de ${comision} usdt`);
-        } else if (max == 'no') {
-            usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a DOGE`));
-            if (usdt <= balance) {
-                conversion = cuenta.convertirADoge(usdt);
-                alert(`${cuenta.convertirADoge(usdt)} DOGE con una comisión de ${comision} usdt`);
-            } else {
-                usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a DOGE`));
-                 conversion = cuenta.convertirADoge(usdt)
-                alert(`${cuenta.convertirADoge(usdt)} DOGE con una comisión de ${comision} usdt`);
-            };
-        };
-        break;
-
-    case `DOT`:
-        max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-        if (max == 'si') {
-            conversion = cuenta.convertirADot(balance);
-            alert(`${cuenta.convertirADot(balance)} DOT con una comisión de ${comision} usdt`);
-        } else if (max == 'no') {
-            usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a DOT`));
-            if (usdt <= balance) {
-                conversion = cuenta.convertirADot(usdt);
-                alert(`${cuenta.convertirADot(usdt)} DOT con una comisión de ${comision} usdt`);
-            } else {
-                usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a DOT`));
-                conversion = cuenta.convertirADot(usdt);
-                alert(`${cuenta.convertirADot(usdt)} DOT con una comisión de ${comision} usdt`);
-            };
-        };
-        break;
-
-        case `SOL`:
-        max = prompt(`¿Quiere convertir todo su balance? si/no`).toLowerCase();
-        if (max == 'si') {
-            conversion = cuenta.convertirASol(balance);
-            alert(`${cuenta.convertirASol(balance)} SOL con una comisión de ${comision} usdt`);
-        } else if (max == 'no') {
-            usdt = Number(prompt(`Ingrese la cantidad de usdt a convertir a SOL`));
-            if (usdt <= balance) {
-                conversion = cuenta.convertirASol(usdt);
-                alert(`${cuenta.convertirASol(usdt)} SOL con una comisión de ${comision} usdt`);
-            } else {
-                usdt = Number(prompt(`Ingrese la cantidad de usdt menora su balance máximo para convertir a SOL`));
-                conversion = cuenta.convertirASol(usdt);
-                alert(`${cuenta.convertirASol(usdt)} SOL con una comisión de ${comision} usdt`);
-            };
-        };
-        break;
-
-    default:
-        alert(`¡Error, no ha seleccionado nada!`);
-        break;
-};
-
+// basicamente primero llamo al DOM para los inputs y para el boton y el select, cada uno con su clase
 const boton = document.querySelector(".convertButton"); 
 
-const input = document.querySelector(".convertInput");
-// entonces si aprieto el boton convertir me muestra la conversion de mis
-// usdt a la crypto que yo elegi con mi switch.
-boton.addEventListener('click', respClick)
-function respClick() {
-    console.log(input.value = conversion);
-}
+const cryptoList = document.querySelector(".cryptoList");
+
+let inputUsdt = document.querySelector(".usdtInput");
+
+let input = document.querySelector(".convertInput");
+
+
+// hago un evento para el select, que a su vez tendra todos los eventos de las conversiones
+cryptoList.addEventListener('change', (evt)=>{
+   const crp = evt.target.value; //guardo el valor del evento en una ctte
+   console.log(`elegiste ${crp}`); // lo muestro para ver si funciona
+   if (crp == 'BTC') { // entonces aca hacemos un if, si eligo btc, procede al sgte evento
+       boton.addEventListener('click', respClick) //si le doy click a convertir donde previamente llene el input de usdt
+       function respClick() { 
+         localStorage.setItem('USDT', inputUsdt.value); //guarda mi cant de usdt
+         usdt = localStorage.getItem('USDT'); // se la agrego a una variable
+         conversion = cuenta.convertirABtc(usdt); //los convierto a btc
+         console.log(usdt); // lo muestro solo para verificara
+         console.log(input.value = conversion); // el input de conversion muestra la convertido.
+         const com = document.querySelector('p'); // por ultimo agrego una p para mostrar la comision
+         com.textContent = `La comision es de ${comision} USDT`;
+        };
+   } else if (crp == 'ETH') { // lo mismo para las demas cryptos
+    boton.addEventListener('click', respClick)
+    function respClick() {
+      localStorage.setItem('USDT', inputUsdt.value);
+      usdt = localStorage.getItem('USDT');
+      conversion = cuenta.convertirAEth(usdt);
+      console.log(usdt);
+      console.log(input.value = conversion);
+      const com = document.querySelector('p');
+      com.textContent = `La comision es de ${comision} USDT`;
+     };
+   } else if (crp == 'ADA') {
+    boton.addEventListener('click', respClick)
+    function respClick() {
+      localStorage.setItem('USDT', inputUsdt.value);
+      usdt = localStorage.getItem('USDT');
+      conversion = cuenta.convertirAAda(usdt);
+      console.log(usdt);
+      console.log(input.value = conversion);
+      const com = document.querySelector('p');
+      com.textContent = `La comision es de ${comision} USDT`;
+     };
+   } else if (crp == 'DOT') {
+    boton.addEventListener('click', respClick)
+    function respClick() {
+      localStorage.setItem('USDT', inputUsdt.value);
+      usdt = localStorage.getItem('USDT');
+      conversion = cuenta.convertirADot(usdt);
+      console.log(usdt);
+      console.log(input.value = conversion);
+      const com = document.querySelector('p');
+      com.textContent = `La comision es de ${comision} USDT`;
+     };
+   } else if (crp == 'DOGE') {
+    boton.addEventListener('click', respClick)
+    function respClick() {
+      localStorage.setItem('USDT', inputUsdt.value);
+      usdt = localStorage.getItem('USDT');
+      conversion = cuenta.convertirADoge(usdt);
+      console.log(usdt);
+      console.log(input.value = conversion);
+      const com = document.querySelector('p');
+      com.textContent = `La comision es de ${comision} USDT`;
+     };
+   } else if (crp == 'SOL') {
+    boton.addEventListener('click', respClick)
+    function respClick() {
+      localStorage.setItem('USDT', inputUsdt.value);
+      usdt = localStorage.getItem('USDT');
+      conversion = cuenta.convertirASol(usdt);
+      console.log(usdt);
+      console.log(input.value = conversion);
+      const com = document.querySelector('p');
+      com.textContent = `La comision es de ${comision} USDT`;
+     };
+   };
+});
+
+
+
+
+// boton.addEventListener('click', respClick)
+// function respClick() {
+//     localStorage.setItem('USDT', inputUsdt.value);
+//     usdt = localStorage.getItem('USDT');
+//     console.log(inputUsdt.value);
+//     console.log(input.value = conversion);
+// }
 
 // buttonClick.addEventListener('click',() => {
 //     console.log(input.value = cuenta.convertirABtc(usdt));
