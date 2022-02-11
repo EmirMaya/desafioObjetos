d416fc91032c88f2dfb39a97c37525c8157fd180
emirMaya <emirmayaok@gmail.com>
2022-02-10 21:27:59 -0300

    desafio jquery

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
 class Cuenta {
-    constructor(nombre, edad, direccion, balance) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.direccion = direccion;
-        this.balance = balance;
-    };
+  constructor(nombre, edad, direccion, balance) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.direccion = direccion;
+    this.balance = balance;
+  };
 
-    convertirAEth(usdt) {
-        //1eth ------ 4000 usdt
-        // x eth ---- tantos usdt
-        //100% --- son la cant de usdt que queremos convertir.
-        // 0.5%--- tantos usdt
-        let eth = 4000;
-        comision = (usdt * 0.5) / 100;
-        this.balance = (usdt - comision) / eth;
-        return this.balance;
-    };
+  convertirAEth(usdt) {
+    //1eth ------ 4000 usdt
+    // x eth ---- tantos usdt
+    //100% --- son la cant de usdt que queremos convertir.
+    // 0.5%--- tantos usdt
+    let eth = 4000;
+    comision = (usdt * 0.5) / 100;
+    this.balance = (usdt - comision) / eth;
+    return this.balance;
+  };
 
-    convertirABtc(usdt) {
-        //1btc------ 40000 usdt
-        // x btc ---- tantos usdt
-        //100% --- son la cant de usdt que queremos convertir.
-        // 0.5%--- tantos usdt
-        let btc = 40000;
-        comision = (usdt * 0.5) / 100;
-        this.balance = (usdt - comision) / btc;
-        return this.balance;
-    };
+  convertirABtc(usdt) {
+    //1btc------ 40000 usdt
+    // x btc ---- tantos usdt
+    //100% --- son la cant de usdt que queremos convertir.
+    // 0.5%--- tantos usdt
+    let btc = 40000;
+    comision = (usdt * 0.5) / 100;
+    this.balance = (usdt - comision) / btc;
+    return this.balance;
+  };
 
-    convertirAAda(usdt) {
-        let ada = 1.4;
-        comision = (usdt * 0.5) / 100;
-        this.balance = (usdt - comision) / ada;
-        return this.balance;
-    };
+  convertirAAda(usdt) {
+    let ada = 1.4;
+    comision = (usdt * 0.5) / 100;
+    this.balance = (usdt - comision) / ada;
+    return this.balance;
+  };
 
-    convertirADot(usdt) {
-        let dot = 41;
-        comision = (usdt * 0.5) / 100;
-        this.balance = (usdt - comision) / dot;
-        return this.balance;
-    };
+  convertirADot(usdt) {
+    let dot = 41;
+    comision = (usdt * 0.5) / 100;
+    this.balance = (usdt - comision) / dot;
+    return this.balance;
+  };
 
-    convertirASol(usdt) {
-        let sol = 77;
-        comision = (usdt * 0.5) / 100;
-        this.balance = (usdt - comision) / sol;
-        return this.balance;
-    };
+  convertirASol(usdt) {
+    let sol = 77;
+    comision = (usdt * 0.5) / 100;
+    this.balance = (usdt - comision) / sol;
+    return this.balance;
+  };
 
-    convertirADoge(usdt) {
-        let doge = 0.6;
-        comision = (usdt * 0.5) / 100;
-        this.balance = (usdt - comision) / doge;
-        return this.balance;
-    };
+  convertirADoge(usdt) {
+    let doge = 0.6;
+    comision = (usdt * 0.5) / 100;
+    this.balance = (usdt - comision) / doge;
+    return this.balance;
+  };
 
 };
 
@@ -70,31 +70,31 @@ const cuenta = new Cuenta('Emir Maya', '24', 'cornu 1234', balance);
 const cryptos = [`BTC`, `ETH`, `ADA`, `DOT`, `DOGE`, `SOL`];
 
 // basicamente primero llamo al DOM para los inputs y para el boton y el select, cada uno con su clase
-const boton = document.querySelector(".convertButton"); 
+const boton = document.querySelector(".convertButton");
 
 const cryptoList = document.querySelector(".cryptoList");
 
 let inputUsdt = document.querySelector(".usdtInput");
 
 let input = document.querySelector(".convertInput");
-
+let crp='';
 
 // hago un evento para el select, que a su vez tendra todos los eventos de las conversiones
-cryptoList.addEventListener('change', (evt)=>{
-   const crp = evt.target.value; //guardo el valor del evento en una ctte
-   console.log(`elegiste ${crp}`); // lo muestro para ver si funciona
-   if (crp == 'BTC') { // entonces aca hacemos un if, si eligo btc, procede al sgte evento
-       boton.addEventListener('click', respClick) //si le doy click a convertir donde previamente llene el input de usdt
-       function respClick() { 
-         localStorage.setItem('USDT', inputUsdt.value); //guarda mi cant de usdt
-         usdt = localStorage.getItem('USDT'); // se la agrego a una variable
-         conversion = cuenta.convertirABtc(usdt); //los convierto a btc
-         console.log(usdt); // lo muestro solo para verificara
-         console.log(input.value = conversion); // el input de conversion muestra la convertido.
-         const com = document.querySelector('p'); // por ultimo agrego una p para mostrar la comision
-         com.textContent = `La comision es de ${comision} USDT`;
-        };
-   } else if (crp == 'ETH') { // lo mismo para las demas cryptos
+cryptoList.addEventListener('change', (evt) => {
+  crp = evt.target.value; //guardo el valor del evento en una ctte
+  console.log(`elegiste ${crp}`); // lo muestro para ver si funciona
+  if (crp == 'BTC') { // entonces aca hacemos un if, si eligo btc, procede al sgte evento
+    boton.addEventListener('click', respClick) //si le doy click a convertir donde previamente llene el input de usdt
+    function respClick() {
+      localStorage.setItem('USDT', inputUsdt.value); //guarda mi cant de usdt
+      usdt = localStorage.getItem('USDT'); // se la agrego a una variable
+      conversion = cuenta.convertirABtc(usdt); //los convierto a btc
+      console.log(usdt); // lo muestro solo para verificara
+      console.log(input.value = conversion); // el input de conversion muestra la convertido.
+      const com = document.querySelector('p'); // por ultimo agrego una p para mostrar la comision
+      com.textContent = `La comision es de ${comision} USDT`;
+    };
+  } else if (crp == 'ETH') { // lo mismo para las demas cryptos
     boton.addEventListener('click', respClick)
     function respClick() {
       localStorage.setItem('USDT', inputUsdt.value);
@@ -104,8 +104,8 @@ cryptoList.addEventListener('change', (evt)=>{
       console.log(input.value = conversion);
       const com = document.querySelector('p');
       com.textContent = `La comision es de ${comision} USDT`;
-     };
-   } else if (crp == 'ADA') {
+    };
+  } else if (crp == 'ADA') {
     boton.addEventListener('click', respClick)
     function respClick() {
       localStorage.setItem('USDT', inputUsdt.value);
@@ -115,8 +115,8 @@ cryptoList.addEventListener('change', (evt)=>{
       console.log(input.value = conversion);
       const com = document.querySelector('p');
       com.textContent = `La comision es de ${comision} USDT`;
-     };
-   } else if (crp == 'DOT') {
+    };
+  } else if (crp == 'DOT') {
     boton.addEventListener('click', respClick)
     function respClick() {
       localStorage.setItem('USDT', inputUsdt.value);
@@ -126,8 +126,8 @@ cryptoList.addEventListener('change', (evt)=>{
       console.log(input.value = conversion);
       const com = document.querySelector('p');
       com.textContent = `La comision es de ${comision} USDT`;
-     };
-   } else if (crp == 'DOGE') {
+    };
+  } else if (crp == 'DOGE') {
     boton.addEventListener('click', respClick)
     function respClick() {
       localStorage.setItem('USDT', inputUsdt.value);
@@ -137,8 +137,8 @@ cryptoList.addEventListener('change', (evt)=>{
       console.log(input.value = conversion);
       const com = document.querySelector('p');
       com.textContent = `La comision es de ${comision} USDT`;
-     };
-   } else if (crp == 'SOL') {
+    };
+  } else if (crp == 'SOL') {
     boton.addEventListener('click', respClick)
     function respClick() {
       localStorage.setItem('USDT', inputUsdt.value);
@@ -148,50 +148,16 @@ cryptoList.addEventListener('change', (evt)=>{
       console.log(input.value = conversion);
       const com = document.querySelector('p');
       com.textContent = `La comision es de ${comision} USDT`;
-     };
-   };
+    };
+  };
 });
 
+//para el desafio de hoy ya hago directamente jquery con event porque me es más conveniente y ademas con selecto accedo al dom
+//cuando convierto muestro una p avisando que se realizo la conversion
+$(document).ready(function () {
+  $('.convertButton').on('click', function () {
+    $( "main" ).append(`<p class="comision">¡Conversión Realizada! Cambiaste tus USDT por ${crp}</p>`);
+  });
+});
 
-
-
-// boton.addEventListener('click', respClick)
-// function respClick() {
-//     localStorage.setItem('USDT', inputUsdt.value);
-//     usdt = localStorage.getItem('USDT');
-//     console.log(inputUsdt.value);
-//     console.log(input.value = conversion);
-// }
-
-// buttonClick.addEventListener('click',() => {
-//     console.log(input.value = cuenta.convertirABtc(usdt));
-// });
-
-
-
-
-
-
-// // creo una lista de crypto
-// let cryptoList = [{nombre: "ETH", precio: 4000 }, 
-// { nombre: "BTC", precio: 40000 },
-// { nombre: "ADA", precio: 1.4 },
-// { nombre: "DOT", precio: 41 },
-// { nombre: "DOGE", precio: 0.6 },
-// { nombre: "SOL", precio: 77 }];
-
-// recorro la lista con un for each
-// for(const crypto of cryptoList){
-// // por cada crypto creo un contenedor
-// let contenedor = document.createElement("div");
-// // cada contenedor tiene un h2 un h3 y un form para convertir
-// contenedor.innerHTML = `<h2>Convertir usdt a ${crypto.nombre}</h2>
-// <h3>El precio hoy es $ ${crypto.precio}</h3>
-//  <form>
-//      <input type="text">
-//      <button>Convertir</button>
-//  </form>`;
- 
-
-// document.body.appendChild(contenedor); }
 
